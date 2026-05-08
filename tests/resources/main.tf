862c4595d22471c1e74a20bded5c03b4268d8dae
Md Anam Raihan <md.anam.raihan@ibm.com>
2026-05-08 19:30:31 +0530

    chore: update sm_tags to resource_tags
    Updated 1 file(s):
    - tests/resources/main.tf

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -30,12 +30,12 @@ module "event_notifications" {
 
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.14.0"
+  version              = "2.15.0"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure
   sm_service_plan      = "trial"
-  sm_tags              = var.resource_tags
+  resource_tags        = var.resource_tags
 }
 
 ##############################################################################
